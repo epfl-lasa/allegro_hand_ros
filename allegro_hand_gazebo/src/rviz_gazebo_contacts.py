fbd2a5bc970afc39d6d71a0b13fc55e4367b9be0
Vaibhav Gupta <guptavaibhav0@gmail.com>
2020-11-26 14:12:51 +0100

    + improved gazebo contact markers

--- allegro_hand_gazebo/src/rviz_gazebo_contacts.py
+++ allegro_hand_gazebo/src/rviz_gazebo_contacts.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Point
 
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 from std_msgs.msg import String
 from gazebo_msgs.msg import ContactsState
@@ -38,7 +39,8 @@ links = [
 
 class BasicMarker():
     def __init__(self, frame, ns, unique_id):
-        self.RGB = [0, 0, 0]
+        self._cmap = plt.cm.get_cmap('autumn_r')
+        self._norm = plt.Normalize(vmin=0.0, vmax=5.0, clip=True)
 
         self.marker_object = Marker()
         self.marker_object.type = Marker.ARROW
@@ -64,30 +66,32 @@ class BasicMarker():
             point.z = z
             self.marker_object.points.append(point)
 
-    def _set_colour(self, R=1.0, G=0.0, B=0.0):
-        self.marker_object.color.r = R
-        self.marker_object.color.g = G
-        self.marker_object.color.b = B
-        # This has to be, otherwise it will be transparent
-        self.marker_object.color.a = 1.0
+    def _set_colour(self, pressure=0.0):
+        rgba = self._cmap(self._norm(pressure))
+        self.marker_object.color.r = rgba[0]
+        self.marker_object.color.g = rgba[1]
+        self.marker_object.color.b = rgba[2]
+        self.marker_object.color.a = rgba[3]
 
     def _set_scale(self,
-                   diameter=0.01,
-                   head_diameter=0.02,
+                   diameter=0.005,
+                   head_diameter=0.008,
                    head_length=0.0):
         self.marker_object.scale.x = diameter
         self.marker_object.scale.y = head_diameter
         self.marker_object.scale.z = head_length
 
-    def update_marker(self, p1, p2):
+    def update_marker(self, p1, p2, pressure):
         if not self.updated:
             self.marker_object.header.stamp = rospy.get_rostime()
             self._set_points([p1, p2])
+            self._set_colour(np.abs(pressure)) 
             self.updated = True
 
     def get_marker(self):
         self.updated = False
         return self.marker_object
+
 
 class ContactsDisplay():
     def __init__(self, ns, links):
@@ -117,26 +121,59 @@ class ContactsDisplay():
 
     def contact_callback(self, data, ii):
         n = len(data.states)
+        if n == 0:
+            self.markers[ii].update_marker([0, 0, 0], [0, 0, 0], 0.0)
+            return
+        
         if n > 0:
-            force_scale = 0.5
-            Fx = data.states[0].total_wrench.force.x * force_scale
-            Fy = data.states[0].total_wrench.force.y * force_scale
-            Fz = data.states[0].total_wrench.force.z * force_scale
+            n_contacts = len(data.states[0].contact_positions)
 
-            Px = data.states[0].contact_positions[0].x
-            Py = data.states[0].contact_positions[0].y
-            Pz = data.states[0].contact_positions[0].z
+            weighted_P = np.zeros(3,)
+            weighted_N = np.zeros(3,)
+            total_normal_force = 0
 
-            self.markers[ii].update_marker([Px, Py, Pz], [Px+Fx, Py+Fy, Pz+Fz])
-        else:
-            self.markers[ii].update_marker([0, 0, 0], [0, 0, 0])
+            for i in range(n_contacts):
+                P = np.asarray([
+                    data.states[0].contact_positions[i].x,
+                    data.states[0].contact_positions[i].y,
+                    data.states[0].contact_positions[i].z,
+                ])
+                N = np.asarray([
+                    data.states[0].contact_normals[i].x,
+                    data.states[0].contact_normals[i].y,
+                    data.states[0].contact_normals[i].z,
+                ])
+                F = np.asarray([
+                    data.states[0].wrenches[i].force.x,
+                    data.states[0].wrenches[i].force.y,
+                    data.states[0].wrenches[i].force.z,
+                ])
+                normal_force = np.dot(N, F)
+                if normal_force > 0:
+                    # only pressure forces
+                    weighted_P += normal_force * P
+                    weighted_N += normal_force * N
+                    total_normal_force += normal_force
+
+            P = weighted_P / total_normal_force
+            N = weighted_N / total_normal_force
+
+            F = np.asarray([
+                data.states[0].total_wrench.force.x,
+                data.states[0].total_wrench.force.y,
+                data.states[0].total_wrench.force.z,
+            ])
+            pressure = total_normal_force# np.dot(F, N)
+
+            temp = P + np.sign(pressure) * N * 0.05
+            self.markers[ii].update_marker(P, temp, pressure)
 
 if __name__ == '__main__':
     rospy.init_node('rviz_gazebo_contacts', anonymous=True)
     rate = rospy.Rate(30)
     
-    ns = rospy.get_namespace()
-    # ns = "/allegro_hand_right/"
+    # ns = rospy.get_namespace()
+    ns = "/allegro_hand_right/"
     contact_display = ContactsDisplay(ns, links)
 
     while not rospy.is_shutdown():
